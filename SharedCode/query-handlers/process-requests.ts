b216832cc5ddd67888d85e61014924176336e5e0
Caleb <caleb.bertrand@verisglobal.com>
2023-01-06 13:53:07 -0600

    removed fuzzysort, and am just taking top 15 results now

--- SharedCode/query-handlers/process-requests.ts
+++ SharedCode/query-handlers/process-requests.ts
@@ -1,11 +1,8 @@
 import { MarketPlaces } from "../marketplaces.enum.js";
 import { RequestResult } from "./handler.js";
 import { steamRequestHandler } from "./steam.js";
-import fuzzysort from "fuzzysort";
 import { amazonRequestHandler } from "./amazon.js";
 import { bestbuyRequestHandler } from "./bestbuy.js";
-
-const sortThreshold = -1750;
 
 export async function processRequest(request: { query: string, marketplaceId: MarketPlaces }): Promise<RequestResult[]> {
     switch(request.marketplaceId) {
@@ -21,7 +18,5 @@ export async function processRequest(request: { query: string, marketplaceId: Ma
 export function filterByConditions(results: Array<RequestResult>, request: { query: string, price: number }, preview = false): Array<RequestResult> {
     const targetPrice = preview ? request.price * 1.5 : request.price;
     const inPriceRange = results.filter(result => result.price <= targetPrice);
-    return fuzzysort.go(request.query, inPriceRange, { key: 'name' })
-        .filter(({ score }, i) => i < 6 || score > sortThreshold)
-        .map(({ obj }) => obj);
+    return inPriceRange.filter((_, i) => i < 15);
 }
